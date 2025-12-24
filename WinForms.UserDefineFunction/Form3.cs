343feb1b58cf551511fef3e760d454387e80079d
Top-Thanwa <phanetramtop@gmail.com>
2025-12-24 19:12:50 +0700

    a

--- WinForms.UserDefineFunction/Form3.cs
+++ WinForms.UserDefineFunction/Form3.cs
@@ -23,16 +23,51 @@ namespace WinForms.UserDefineFunction
             int size = int.Parse(textBox2.Text);
             if (radioButton1.Checked)
             {
-                //textBox3.Text = Triangle1(letter, size);
+                textBox3.Text = Triangle1(letter, size);
             }
             else if (radioButton2.Checked)
             {
-                //textBox3.Text = Triangle2(letter);
+                textBox3.Text = Triangle2(letter);
             }
             else if (radioButton3.Checked)
             {
-                //Triangle3(letter, size, out string result);
+                Triangle3(letter, size, out string result);
+                textBox3.Text = result;
             }
+        }
+
+        private void Triangle3(string letter, int size, out string result)
+        {
+            result = "Triangle3" + Environment.NewLine;
+
+            for (int i = 0; i < size; i++)
+            {
+                result += letter + Environment.NewLine;
+            }
+        }
+
+        private string Triangle2(string letter, int size = 5)
+        {
+            string result = "Triangle2" + Environment.NewLine;
+
+            for (int i = 0; i < size; i++)
+            {
+                result += letter + Environment.NewLine;
+            }
+
+            return result;
+        }
+
+        private string Triangle1(string letter, int size)
+        {
+            string result = "Triangle1" + Environment.NewLine;
+
+            for (int i = 0; i < size; i++)
+            {
+                result += letter + Environment.NewLine;
+            }
+
+            return result;
         }
     }
 }
